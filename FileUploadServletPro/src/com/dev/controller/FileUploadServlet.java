3153f03b22cfb96f71cdc412e441fab64fb1f324
Debasish <debasishpradhan890@gmail.com>
2020-07-10 21:46:26 +0530

    servlet & jsp page created

---
+++ FileUploadServletPro/src/com/dev/controller/FileUploadServlet.java
@@ -0,0 +1,64 @@
+package com.dev.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@WebServlet("/FileUploadServlet")
+@MultipartConfig
+public class FileUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final String UPLOAD_DIRECTORY = "";
+
+	public FileUploadServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		System.out.println(getServletContext().getRealPath("/WEB-INF/file"));
+		
+		
+		if (ServletFileUpload.isMultipartContent(request)) {
+			try {
+				List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+				for (FileItem item : multiparts) {
+					if (!item.isFormField()) {
+						String name = new File(item.getName()).getName();
+						item.write(new File(getServletContext().getRealPath("/WEB-INF/file") + File.separator + name));
+					}
+				}
+				// File uploaded successfully
+				request.setAttribute("message", "File Uploaded Successfully");
+			} catch (Exception ex) {
+				request.setAttribute("message", "File Upload Failed due to " + ex);
+			}
+
+		} else {
+			request.setAttribute("message", "Sorry this Servlet only handles file upload request");
+		}
+
+		request.getRequestDispatcher("/result.jsp").forward(request, response);
+
+	}
+
+}
